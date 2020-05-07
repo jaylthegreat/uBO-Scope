665b43ca7c5d676d227593c33b38975504fd6a0d
jaylthegreat <jaylthegreat1123@gmail.com>
2020-05-07 02:50:04 -0400

    moved categorizing functionality outside of drawChart. added functionality to filter by categories. improved UI, made more descriptive. I think that's it for now.

--- js/sankey.js
+++ js/sankey.js
@@ -1,35 +1,50 @@
 google.charts.load('current', {'packages':['sankey']});
 // google.charts.setOnLoadCallback(drawChart);
-google.charts.setOnLoadCallback(function() {
-    drawChart(JSON.parse(jsonData))
-});
-let showCategories =     true;
-let showOnlyCategories = true;
-let filterList = ""
+// google.charts.setOnLoadCallback(function() {
+//     drawChart(JSON.parse(jsonData))
+// });
+let showCategories =     false;
+let showOnlyCategories = false;
+
 
 //set functionality for buttons
 let showCatsToggle = document.getElementById("showCatsToggle");
 let onlyCatsToggle = document.getElementById("onlyCatsToggle");
 let filtersInputStarts = document.getElementById("filtersInputStartpoints");
 let filtersInputEnds = document.getElementById("filtersInputEndpoints");
+let filtersCats = document.getElementById("filtersCats");
+
 let applyBtn = document.getElementById("applyBtn");
 
 showCatsToggle.checked = showCategories;
 onlyCatsToggle.checked = showOnlyCategories;
+
 
 applyBtn.onclick = function (element) {
     showCategories = showCatsToggle.checked;
     showOnlyCategories = onlyCatsToggle.checked;
     filterListStarts = filtersInputStarts.value;
     filterListEnds = filtersInputEnds.value;
-    console.log("filters are: " + filterListStarts);
-    // now redraw chart!
-    // drawChartForOnlyOneEndpoint(filterList);
+    filterListCats = filtersCats.value;
+    // you must turn on categories if you want to filter by category
+    if (filterListCats != "" ) {
+        showCategories = true;
+        showCatsToggle.checked = showCategories;
+    }
     
     filteredData = filterDataByStartpoints(filterListStarts,jsonData);
     // this may be the most inefficient possible way of doing this
-    filteredFilteredData = filterDataByEndpoints(filterListEnds,JSON.stringify(filteredData));
-    drawChart(filteredFilteredData);
+    filteredData = filterDataByEndpoints(filterListEnds,JSON.stringify(filteredData));
+    if (showCategories) {
+        filteredData = filterDataIntoCategories(JSON.stringify(filteredData));
+        //if you want to filter by category, filter by category
+        if (filterListCats != "") {
+            filteredData = filterDataByCategory(filterListCats, JSON.stringify(filteredData));
+        }
+    }
+
+    // now redraw chart!
+    drawChart(filteredData);
 }
 
 ////////// makesure the checkboxes stay sane ///
@@ -53,7 +68,7 @@ showCatsToggle.onclick = function (element) {
 /////////////////////////////////////////////////
 
 function filterDataByStartpoints(startpoints, inputData) {
-    console.log(inputData);
+    // console.log(inputData);
     pointsToShow = startpoints.split(" ");
     var newThing = JSON.parse(inputData, function (key, value) {
         // if pass in empty string default to use normal dataset.
@@ -81,7 +96,94 @@ function filterDataByStartpoints(startpoints, inputData) {
     // drawChart(newThing);
 }
 
+function filterDataIntoCategories(inputData) {
+    var newThing = JSON.parse(inputData, function (key, value) {
+        if (typeof value != 'string') {
+            return value;
+        }
+        var element = value.split(" ");
+        if (element.length != 2) {
+            return value;
+        }
+        // now we check what category it would be in and deal with it
+        let thisCategory = "Unknown";
+        let categories = services["categories"];
+                    let found = false;
+                    if (element[0].includes("github")) {
+                        thisCategory = "GitHub";
+                        found = true;
+                    } else if (element[0].includes("google")) {
+                        thisCategory = "Google";
+                        found = true;
+                    } else if (element[0].includes("aws") || element[1].includes("amazon")) {
+                        thisCategory = "Amazon";
+                        found = true;
+                    } else if (element[0].includes("cloudfront")) {
+                        thisCategory = "Cloudfront";
+                        found = true;
+                    }
+                    for (category in categories) {
+                        if (found) {
+                            break;
+                        } else {
+                            let companies = categories[category]; 
+                            for (company in companies) {
+                                let websites = companies[company];
+                                for (website in websites) {
+                                    let whyAreThereSoManyCategories = websites[website];
+                                    for (why in whyAreThereSoManyCategories) {
+                                        let domains = whyAreThereSoManyCategories[why];
+                                        if (domains.includes(element[0])) {
+                                            if (category == "Disconnect") {
+                                                thisCategory = website;
+                                            } else {
+                                                thisCategory = category;
+                                            }
+                                            found = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+        // if recognized, put into a category
+        if (thisCategory != "Unknown") {
+            element[0] = thisCategory;
+        } 
+        // if unrecognized, only set category if user has asked to only display categorized things
+        else if (showOnlyCategories) {
+            element[0] = thisCategory;
+        }
+        return element[0] + " " + element[1];
+        
+    });
 
+    return newThing;
+}
+
+function filterDataByCategory(categories, inputData) {
+    catsToShow = categories.split(" ");
+    var newThing = JSON.parse(inputData, function (key, value) {
+        // if pass in empty string default to use normal dataset.
+        if (categories == "") {
+            return value;
+        }
+        if (typeof value != 'string') {
+            return value;
+        }
+        var splitVal = value.split(" ");
+        if (splitVal.length != 2) {
+            return value;
+        }
+        for (i = 0; i < catsToShow.length; i++) {
+            if (splitVal[0] == catsToShow[i]) {
+                return value;
+            }    
+        }
+        
+    });
+    return newThing;
+}
 
 function filterDataByEndpoints(endpoints, inputData) {
     pointsToShow = endpoints.split(" ");
@@ -167,62 +269,8 @@ function drawChart(parsedJsonData) {
                 element[0] = element[1];
                 element[1] = temp;
                 element.push(1);
-                let thisCategory = "Unknown";
-                if (showCategories) {
-                    let categories = services["categories"];
-                    let found = false;
-                    if (element[1].includes("github")) {
-                        thisCategory = "GitHub";
-                        found = true;
-                    } else if (element[1].includes("google")) {
-                        thisCategory = "Google";
-                        found = true;
-                    } else if (element[1].includes("aws") || element[1].includes("amazon")) {
-                        thisCategory = "Amazon";
-                        found = true;
-                    } else if (element[1].includes("cloudfront")) {
-                        thisCategory = "Cloudfront";
-                        found = true;
-                    }
-                    for (category in categories) {
-                        if (found) {
-                            break;
-                        } else {
-                            let companies = categories[category]; 
-                            for (company in companies) {
-                                let websites = companies[company];
-                                for (website in websites) {
-                                    let whyAreThereSoManyCategories = websites[website];
-                                    for (why in whyAreThereSoManyCategories) {
-                                        let domains = whyAreThereSoManyCategories[why];
-                                        if (domains.includes(element[1])) {
-                                            if (category == "Disconnect") {
-                                                thisCategory = website;
-                                            } else {
-                                                thisCategory = category;
-                                            }
-                                            found = true;
-                                            //console.log("Found", element[1],why,website,company,category)
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                
                 if (!firstParties.includes(element[1])) {
-                    // only replace with category if you are showing categories
-                    if (showCategories) {
-                        if (showOnlyCategories) {
-                            // console.log("Replacing",element[1],thisCategory);
-                            element[1] = thisCategory;
-                        }
-                        // if not using only categories, only replace those you know
-                        else if (thisCategory !== "Unknown") {
-                            // console.log("Replacing",element[1],thisCategory);
-                            element[1] = thisCategory;
-                        }
-                    }
                     data.addRows([element]);
                 }
             });
