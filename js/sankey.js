747402632dfb582f31e3915bc20635029eb493b9
jaylthegreat <jaylthegreat1123@gmail.com>
2020-05-06 22:46:43 -0400

    created function that filters json data so that graph only draws data from one endpoint

--- js/sankey.js
+++ js/sankey.js
@@ -1,14 +1,40 @@
 google.charts.load('current', {'packages':['sankey']});
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(function() {
+    drawChart(JSON.parse(jsonData))
+});
 
-function drawChart() {
+function drawChartForOnlyOneEndpoint(endpoint) {
+    console.log("do a thing");
+    var newThing = JSON.parse(jsonData, function (key, value) {
+        if (typeof value != 'string') {
+            return value;
+        }
+        var splitVal = value.split(" ");
+        if (splitVal.length != 2) {
+            return value;
+        }
+        if (splitVal[1] == endpoint) {
+            //so if a string with two parts and the first equals our endpoint, return
+            console.log("use this one!");
+            return value;
+        }
+    });
+    console.log("new data is" + newThing);
+    console.log(newThing);
+    // console.log("old data is : ");
+    // console.log(JSON.parse(jsonData));
+    drawChart(newThing);
+}
+
+function drawChart(parsedJsonData) {
 var data = new google.visualization.DataTable();
 data.addColumn('string', 'From');
 data.addColumn('string', 'To');
 data.addColumn('number', 'Weight');
-
+console.log(parsedJsonData);
 //jsonData should be set in a previous script
-let scopeData = JSON.parse(jsonData);
+let scopeData = parsedJsonData;
 let includeBlocked = true;
 for (let thing in scopeData) {
     if (thing.includes("month")) {
